a66b15f440c4a5894e8edd357af5f297dcf1e85f
Tsele Ramoroke <42624079+Ramoroke@users.noreply.github.com>
2024-04-19 18:14:29 +0200

    Create data.js

---
+++ src/data/data.js
@@ -0,0 +1,47 @@
+export const vintageCars = [
+    {
+        name: "Beetle",
+        year: 1968,
+        model: "VW",
+        description: "The Tesla Model S is an all-electric luxury sedan produced by Tesla, Inc.",
+        src: "assets/models/vintage/Beetle.obj"
+    },
+    {
+        name: "Maybach",
+        year: 1957,
+        model: "Maybach Zeplin",
+        description: "The Tesla Model S is an all-electric luxury sedan produced by Tesla, Inc.",
+        src: "assets/models/vintage/Maybach.obj"
+    },
+    {
+        name: "Samba",
+        year: 1969,
+        model: "VW",
+        description: "The Tesla Model S is an all-electric luxury sedan produced by Tesla, Inc.",
+        src: "assets/models/vintage/samba.obj"
+    },
+];
+
+export const modernCars = [
+    {
+        name: "Mercedes",
+        year: 2019,
+        model: "Maybach Zeplin",
+        description: "The Tesla Model S is an all-electric luxury sedan produced by Tesla, Inc.",
+        src: "assets/models/modern/MercedesG.obj"
+    },
+    {
+        name: "Cadillac",
+        year: 2022,
+        model: "VW",
+        description: "The Tesla Model S is an all-electric luxury sedan produced by Tesla, Inc.",
+        src: "assets/models/modern/cadillac.obj"
+    },
+    {
+        name: "Cartrera",
+        year: 2016,
+        model: "Mercedce",
+        description: "The Tesla Model S is an all-electric luxury sedan produced by Tesla, Inc.",
+        src: "assets/models/modern/cartrera.obj"
+    },
+];
